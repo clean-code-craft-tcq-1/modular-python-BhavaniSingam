8452ea96bfd1791e8a26541641891c00c102a6cd
BhavaniSingam <39860323+BhavaniSingam@users.noreply.github.com>
2021-02-22 19:02:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import Colorpairs_Unittests as cput
 if __name__ == '__main__':
-  test_number_to_pair(4, 'White', 'Brown')
-  test_number_to_pair(5, 'White', 'Slate')
-  test_pair_to_number('Black', 'Orange', 12)
-  test_pair_to_number('Violet', 'Slate', 25)
-  test_pair_to_number('Red', 'Orange', 7)
+  cput.test_number_to_pair(4, 'White', 'Brown')
+  cput.test_number_to_pair(5, 'White', 'Slate')
+  cput.test_pair_to_number('Black', 'Orange', 12)
+  cput.test_pair_to_number('Violet', 'Slate', 25)
+  cput.test_pair_to_number('Red', 'Orange', 7)
   print('Done :)')
