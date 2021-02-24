d4d136bbf5d781fadc5b21749b5fb119d8aa5d0f
BhavaniSingam <39860323+BhavaniSingam@users.noreply.github.com>
2021-02-24 15:22:13 +0530

    Update ColorCodeRefManual.py

--- ColorCodeRefManual.py
+++ ColorCodeRefManual.py
@@ -1,7 +1,8 @@
+import ColorPairsGetindex
 import ColorPairsCreation
 
 print ("{:<10} {:<10} {:<10}".format('Pair Number','Major Color', 'Minor Color'))
-for majorcolor in ColorPairsCreation.MAJOR_COLORS:
-  for minorcolor in ColorPairsCreation.MINOR_COLORS:
+for majorcolor in ColorPairsGetindex.MAJOR_COLORS:
+  for minorcolor in ColorPairsGetindex.MINOR_COLORS:
     pairnumber = ColorPairsCreation.get_pair_number_from_color(majorcolor,minorcolor)
     print ("{:<10} {:<10} {:<10}".format(pairnumber, majorcolor, minorcolor))
