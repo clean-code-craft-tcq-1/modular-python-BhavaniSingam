78e15bb967baf2490f3073039c156525bed8069b
BhavaniSingam <39860323+BhavaniSingam@users.noreply.github.com>
2021-02-26 10:43:14 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import ColorPairsUnittests as cput
-if __name__ == '__main__':
-  cput.test_number_to_pair(4, 'White', 'Brown')
-  cput.test_number_to_pair(5, 'White', 'Slate')
-  cput.test_pair_to_number('Black', 'Orange', 12)
-  cput.test_pair_to_number('Violet', 'Slate', 25)
-  cput.test_pair_to_number('Red', 'Orange', 7)
-  print('Done :)')
